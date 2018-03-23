2b0f78949ba6bb8af23f5579714308582d0a457b
bobmartinjones <bobmartinjones@outlook.com>
2018-03-23 21:48:27 +0000

    Added the updated app.component and app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { MovieDb } from "./movies/interfaces/movie-themoviedb.interface";
+import { MovieService } from "./services/movies.service";
+import { UtilService } from "./shared/util.service";
+import { Subscription } from "rxjs/Subscription";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private movies: MovieDb[] = [];
+  private sub1: Subscription;
+
+  constructor(
+    private _movieService: MovieService,
+    private _utilService: UtilService
+  ) { }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  ngOnDestroy(): void {
+    this.sub1 && this.sub1.unsubscribe();
+  }
   title = 'app';
+  starsCount: number;
+  starsCounts: number[] = [];
+  errorMessage:string;
+
+  getMovies(page: number = 1): void {
+    this.sub1 = this._movieService.getMovies().subscribe((movies: MovieDb[]) => {
+      // console.log("movies", movies);
+      if (movies) {
+        this.movies.push(...movies);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RatingModule } from './shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { MovieTileComponent } from './movies/components/movie-tile-component/movie-tile.component';
+import { MovieService } from './services/movies.service';
+import { UtilService } from './shared/util.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieTileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RatingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MovieService,
+    UtilService
+  ],
+  bootstrap: [AppComponent],
+
+
 })
 export class AppModule { }
