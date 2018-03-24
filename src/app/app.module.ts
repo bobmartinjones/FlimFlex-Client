52563b5e22671724730977b3298ad002d1591902
bobmartinjones <bobmartinjones@outlook.com>
2018-03-24 12:42:31 +0000

    Fixed star.component import in the app.module.ts after moving it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RatingModule } from './shared/star.component';
+
 import { FormsModule } from '@angular/forms';
 import { MovieTileComponent } from './movies/components/movie-tile-component/movie-tile.component';
 import { MovieService } from './services/movies.service';
 import { UtilService } from './shared/util.service';
+import { RatingModule } from './shared/components/star-component/star.component';
 
 
 @NgModule({
